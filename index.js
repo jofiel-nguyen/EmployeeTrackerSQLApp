e6eff0eb3f202aee292cd68f3b129dd3f57f7684
wren <zennguyen2511@gmail.com>
2023-05-11 18:38:04 -0500

    add index and schema

---
+++ index.js
@@ -0,0 +1,56 @@
+// import required modules and establish database connection
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'company_db'
+});
+
+// function to add a role
+function addRole() {
+  // query the database for existing departments
+  connection.query('SELECT id, name FROM department', (err, res) => {
+    if (err) throw err;
+
+    // prompt user for role information
+    inquirer.prompt([
+      {
+        name: 'title',
+        type: 'input',
+        message: 'Enter the title of the role:'
+      },
+      {
+        name: 'salary',
+        type: 'number',
+        message: 'Enter the salary for the role:'
+      },
+      {
+        name: 'department',
+        type: 'list',
+        message: 'Select the department for the role:',
+        choices: res.map(department => ({
+          name: department.name,
+          value: department.id
+        }))
+      }
+    ]).then(answers => {
+      // insert the new role into the database
+      connection.query(
+        'INSERT INTO role SET ?',
+        {
+          title: answers.title,
+          salary: answers.salary,
+          department_id: answers.department
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`New role "${answers.title}" added to database.`);
+          // return to main menu
+          mainMenu();
+        }
+      );
+    });
+  });
+}
